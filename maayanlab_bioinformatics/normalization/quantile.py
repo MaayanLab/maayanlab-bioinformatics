cd8ce29fa89e563137a13147485626419d569cb8
Daniel Clarke <u8sand@gmail.com>
2020-05-22 09:11:15 -0400

    Lazy load h5py in quantileNormalize_h5 function

--- maayanlab_bioinformatics/normalization/quantile.py
+++ maayanlab_bioinformatics/normalization/quantile.py
@@ -35,47 +35,42 @@ def quantileNormalize_pd(mat: pd.DataFrame):
     columns=mat.columns,
   )
 
-try:
-  import h5py
-except:
-  pass
-else:
+def quantileNormalize_h5(in_mat, out_mat, tmp=None):
   import os, tempfile, h5py
-  def quantileNormalize_h5(in_mat: h5py.Dataset, out_mat: h5py.Dataset, tmp=None):
-    '''
-    Maximum memory required (3 * in_mat.shape[1] * sizeof(dtype))
-    Storage required 4 * in_mat.size
-      - input matrix
-      - transposed copy
-      - sorted & transposed copy
-      - output matrix
-    '''
-    assert isinstance(in_mat, h5py.Dataset)
-    assert isinstance(out_mat, h5py.Dataset)
-    assert in_mat.shape == out_mat.shape
-    # transpose + sort
-    tmp_f = tempfile.mktemp() if tmp is None else tmp
-    tmp_h5 = h5py.File(tmp_f, 'w')
-    tmp_T_mat = tmp_h5.create_dataset('tmp_T', shape=(in_mat.shape[1], in_mat.shape[0]), dtype=in_mat.dtype)
-    tmp_T_sorted_mat = tmp_h5.create_dataset('tmp_T_sorted', shape=(in_mat.shape[1], in_mat.shape[0]), dtype=in_mat.dtype)
-    sorted_col_vec_agg_rank = np.zeros(in_mat.shape[0])
-    for col in range(in_mat.shape[1]):
-      # this single read is potentially expensive but the two writes are cheap
-      col_vec = in_mat[:, col]
-      tmp_T_mat[col, :] = col_vec
-      sorted_col_vec = np.sort(col_vec)
-      tmp_T_sorted_mat[col, :] = sorted_col_vec
-      sorted_col_vec_agg_rank += sorted_col_vec
-    # setup rank matrix
-    sorted_col_vec_agg_rank /= in_mat.shape[1]
-    # construct output matrix
-    for c in range(in_mat.shape[1]):
-      # this write is potentially expensive but the reads are cheap
-      out_mat[:, c] = [
-        sorted_col_vec_agg_rank[i]
-        for i in np.searchsorted(tmp_T_sorted_mat[c, :], tmp_T_mat[c, :])
-      ]
-    # close and remove tmp file
-    tmp_h5.close()
-    os.remove(tmp_f)
-    return out_mat
+  '''
+  Maximum memory required (3 * in_mat.shape[1] * sizeof(dtype))
+  Storage required 4 * in_mat.size
+    - input matrix
+    - transposed copy
+    - sorted & transposed copy
+    - output matrix
+  '''
+  assert isinstance(in_mat, h5py.Dataset)
+  assert isinstance(out_mat, h5py.Dataset)
+  assert in_mat.shape == out_mat.shape
+  # transpose + sort
+  tmp_f = tempfile.mktemp() if tmp is None else tmp
+  tmp_h5 = h5py.File(tmp_f, 'w')
+  tmp_T_mat = tmp_h5.create_dataset('tmp_T', shape=(in_mat.shape[1], in_mat.shape[0]), dtype=in_mat.dtype)
+  tmp_T_sorted_mat = tmp_h5.create_dataset('tmp_T_sorted', shape=(in_mat.shape[1], in_mat.shape[0]), dtype=in_mat.dtype)
+  sorted_col_vec_agg_rank = np.zeros(in_mat.shape[0])
+  for col in range(in_mat.shape[1]):
+    # this single read is potentially expensive but the two writes are cheap
+    col_vec = in_mat[:, col]
+    tmp_T_mat[col, :] = col_vec
+    sorted_col_vec = np.sort(col_vec)
+    tmp_T_sorted_mat[col, :] = sorted_col_vec
+    sorted_col_vec_agg_rank += sorted_col_vec
+  # setup rank matrix
+  sorted_col_vec_agg_rank /= in_mat.shape[1]
+  # construct output matrix
+  for c in range(in_mat.shape[1]):
+    # this write is potentially expensive but the reads are cheap
+    out_mat[:, c] = [
+      sorted_col_vec_agg_rank[i]
+      for i in np.searchsorted(tmp_T_sorted_mat[c, :], tmp_T_mat[c, :])
+    ]
+  # close and remove tmp file
+  tmp_h5.close()
+  os.remove(tmp_f)
+  return out_mat
