2c2a2ea480f210e323b26b7443f2d1424476b4bc
Daniel Clarke <u8sand@gmail.com>
2020-05-22 09:18:06 -0400

    Make merge work with empty dfs

--- maayanlab_bioinformatics/utils/merge.py
+++ maayanlab_bioinformatics/utils/merge.py
@@ -1,9 +1,15 @@
 import pandas as pd
 
-def merge(left, *rights, **kwargs):
+def merge(*dfs, **kwargs):
   ''' Helper function for many trivial (index based) joins
   '''
+  if not dfs:
+    return pd.DataFrame()
+  #
+  left, *rights = dfs
+  #
   merged = left
   for right in rights:
     merged = pd.merge(left=merged, left_index=True, right=right, right_index=True, **kwargs)
+  #
   return merged
