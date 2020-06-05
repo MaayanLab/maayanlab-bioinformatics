cdc595980f5c06636e9e700aff5f9b70ac48d668
Daniel Clarke <u8sand@gmail.com>
2020-06-05 12:52:13 -0400

    Added log10_normalize

--- maayanlab_bioinformatics/normalization/log.py
+++ maayanlab_bioinformatics/normalization/log.py
@@ -19,3 +19,20 @@ def log2_normalize_np(mat: np.ndarray, offset=1.):
 @log2_normalize.register
 def log2_normalize_pd(mat: pd.DataFrame, offset=1.):
   return np.log2(mat + offset)
+
+
+@singledispatch
+def log10_normalize(mat, offset=1.):
+  ''' Compute log normalization of matrix
+  Simple `log10(x + offset)`, offset usually set to 1. because log(0) is undefined.
+  '''
+  logging.warn('Unrecognized type: ' + type(mat).__name__)
+  return log10_normalize_np(mat, offset=offset)
+
+@log10_normalize.register
+def log10_normalize_np(mat: np.ndarray, offset=1.):
+  return np.log10(mat + offset)
+
+@log10_normalize.register
+def log10_normalize_pd(mat: pd.DataFrame, offset=1.):
+  return np.log10(mat + offset)
