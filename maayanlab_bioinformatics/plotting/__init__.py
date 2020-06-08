5c55d8f7c66d167849dd8bcb3f749ec24eca850d
Daniel Clarke <u8sand@gmail.com>
2020-06-08 18:48:58 -0400

    Added bridge plot helper function

---
+++ maayanlab_bioinformatics/plotting/__init__.py
@@ -0,0 +1,4 @@
+''' This module contains various helpers for plotting things
+'''
+
+from maayanlab_bioinformatics.plotting.bridge import bridge_plot
---
+++ maayanlab_bioinformatics/plotting/bridge.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+def bridge_plot(select: pd.Series):
+  ''' Use the filter to construct a bridge plot.
+
+  ```python
+  import numpy as np
+  from matplotlib import pyplot as plt
+  from maayanlab_bioinformatics.plotting import bridge_plot
+
+  x, y = bridge_plot(select)
+  plt.plot(x, y)
+  plt.vlines(np.where(select, 1, 0), ymin=-1, ymax=0)
+  plt.show()
+  ```
+
+  :param select: (pd.Series) selection of hits (i.e. `df['gene'] == 'my_target'`)
+  :return: (Tuple[np.array, np.array]) x and y arrays ready to be plotted.
+  '''
+  up = (~select).sum() / select.shape[0]
+  dn = -(1-up)
+  x = np.arange(select.shape[0])
+  y = np.cumsum(select.apply(lambda s: up if s else dn))
+  return x, y
