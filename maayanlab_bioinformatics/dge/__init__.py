ba9684711d6de72d1344c9846c58332a6b6435ab
Daniel Clarke <u8sand@gmail.com>
2020-06-08 18:48:22 -0400

    Minor module description fix

--- maayanlab_bioinformatics/dge/__init__.py
+++ maayanlab_bioinformatics/dge/__init__.py
@@ -1,4 +1,4 @@
-''' This module contains functions differential expression analysis
+''' This module contains functions for differential expression analysis
 '''
 
 from maayanlab_bioinformatics.dge.characteristic_direction import characteristic_direction, up_down_from_characteristic_direction
