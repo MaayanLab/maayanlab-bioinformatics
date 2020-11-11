655f56b1862d16202122009b77e0f47143ab09c0
Daniel Clarke <u8sand@gmail.com>
2020-11-11 10:50:20 -0500

    Release new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='maayanlab_bioinformatics',
-  version='0.1.2',
+  version='0.1.3',
   url='https://github.com/maayanLab/maayanlab-bioinformatics/',
   author='Daniel J. B. Clarke',
   author_email='u8sand@gmail.com',
