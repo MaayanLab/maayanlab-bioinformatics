86e885fd179c1e72cd6ee4d9ea1340c20df8d8ac
Daniel Clarke <u8sand@gmail.com>
2021-01-19 13:01:26 -0500

    Publish new minor version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='maayanlab_bioinformatics',
-  version='0.1.3',
+  version='0.2.0',
   url='https://github.com/maayanLab/maayanlab-bioinformatics/',
   author='Daniel J. B. Clarke',
   author_email='u8sand@gmail.com',
