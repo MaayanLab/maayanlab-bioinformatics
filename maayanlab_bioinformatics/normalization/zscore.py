3c5d3d2f8d2b33cdb1362d2492b84e9c7e585de9
Daniel Clarke <u8sand@gmail.com>
2021-04-13 12:03:22 -0400

    Eliminate slightly misleading zscore docs

--- maayanlab_bioinformatics/normalization/zscore.py
+++ maayanlab_bioinformatics/normalization/zscore.py
@@ -9,23 +9,6 @@ from scipy.stats import zscore
 def zscore_normalize(mat, ddof=0):
   ''' Compute the z score of each value in the sample, relative to the sample mean and standard deviation.
   In the case of a pd.DataFrame, preserve the index on the output frame.
-  e.g.
-        |condition_1|condition_2|...|condition_n|
-  gene_1|    1      |     1     |...|    100    |
-  gene_2|    1      |    100    |...|     1     |
-
-  You'd get, approximately:
-
-        |condition_1|condition_2|...|condition_n|
-  gene_1| -1 (<mu)  |  -1 (<mu) |...|  3 (>>mu) |
-  gene_2| -1 (<mu)  |  3 (>>mu) |...| -1 (<mu)  |
-
-  That-is, samples disproportionately represented
-   in a specific condition become >> 0 and those
-   disproportionately under-represented become << 0.
-
-  If you were to plot any given sample's distribution,
-   it should now be normal.
   '''
   logging.warn('Unrecognized type: ' + type(mat).__name__)
   return zscore_normalize_np(mat, ddof=ddof)
