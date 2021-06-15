5eb918167c050a26e16aa9fe2ca5bc8a01f8f453
Daniel Clarke <u8sand@gmail.com>
2021-06-15 18:03:39 -0400

    Remove quantile_normalize_h5

--- maayanlab_bioinformatics/normalization/__init__.py
+++ maayanlab_bioinformatics/normalization/__init__.py
@@ -4,5 +4,5 @@
 from maayanlab_bioinformatics.normalization.cpm import cpm_normalize
 from maayanlab_bioinformatics.normalization.filter import filter_by_var, filter_by_expr
 from maayanlab_bioinformatics.normalization.log import log2_normalize, log10_normalize
-from maayanlab_bioinformatics.normalization.quantile import quantile_normalize, quantile_normalize_h5
+from maayanlab_bioinformatics.normalization.quantile import quantile_normalize
 from maayanlab_bioinformatics.normalization.zscore import zscore_normalize
