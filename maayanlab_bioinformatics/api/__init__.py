d2a0c954db9f144a791b2027f2aca71509376ef1
Daniel Clarke <u8sand@gmail.com>
2022-01-11 18:00:29 -0500

    Object oriented Enrichr API

--- maayanlab_bioinformatics/api/__init__.py
+++ maayanlab_bioinformatics/api/__init__.py
@@ -1,4 +1,4 @@
 '''This module contains API wrappers for some commonly used tool APIs
 '''
 
-from maayanlab_bioinformatics.api.enrichr import enrichr_link_from_genes, enrichr_get_top_results, enrichr_term_genes
+from maayanlab_bioinformatics.api.enrichr import enrichr_link_from_genes, enrichr_get_top_results, enrichr_term_genes, EnrichrUserList
--- maayanlab_bioinformatics/api/enrichr.py
+++ maayanlab_bioinformatics/api/enrichr.py
@@ -45,3 +45,96 @@ def enrichr_term_genes(bg, terms, enrichr_link='https://maayanlab.cloud/Enrichr'
       resp.text,
     ))
   return resp.json()
+
+class EnrichrUserList:
+  ''' Object oriented access to Enrichr results.
+
+  Example:
+  ```python
+  from maayanlab_bioinformatics.api import EnrichrUserList
+  mylist = EnrichrUserList([
+    'STAT1', 'ACE2', #...
+  ], 'mylist')
+  print(mylist.link)
+  mylist['GO_Biological_Process_2021'] # returns dataframe with enrichment results
+  ```
+  '''
+  def __init__(self, genes, description='', enrichr_link='https://maayanlab.cloud/Enrichr'):
+    self._enrichr_link = enrichr_link
+    self._genes = genes
+    self._description = description
+    self._shortId = None
+    self._userListId = None
+    self._results = {}
+
+  def __repr__(self):
+    return f"EnrichrUserList(..., description={repr(self.description)})"
+  
+  @property
+  def genes(self):
+    return self._genes
+
+  @property
+  def description(self):
+    return self._description
+
+  @property
+  def link(self):
+    return enrichr_link + '/enrich?dataset=' + self.shortId
+
+  @property
+  def shortId(self):
+    if self._shortId is None: self._addList()
+    return self._shortId
+
+  @property
+  def userListId(self):
+    if self._userListId is None: self._addList()
+    return self._userListId
+  
+  def __getitem__(self, library):
+    if library not in self._results: self._enrich(library)
+    return self._results[library].copy()
+
+  def _addList(self):
+    resp = requests.post(self._enrichr_link + '/addList', files={
+      'list': (None, '\n'.join(self.genes)),
+      'description': (None, self.description),
+    })
+    if resp.status_code != 200:
+      raise Exception('Enrichr failed with status {}: {}'.format(
+        resp.status_code,
+        resp.text,
+      ))
+    results = resp.json()
+    # wait a tinybit before returning link (backoff)
+    time.sleep(1)
+    #
+    self._userListId = results['userListId']
+    self._shortId = results['shortId']
+  
+  def _enrich(self, library):
+    resp = requests.get(self._enrichr_link + '/enrich', params={
+      'userListId': self.userListId,
+      'backgroundType': library,
+    })
+    if resp.status_code != 200:
+      raise Exception('Enrichr failed with status {}: {}'.format(
+        resp.status_code,
+        resp.text,
+      ))
+    results = resp.json()
+    # wait a tinybit before returning link (backoff)
+    time.sleep(1)
+    self._results[library] = pd.DataFrame([
+      dict(
+        rank=rank,
+        term=term,
+        pvalue=pvalue,
+        zscore=zscore,
+        combinedscore=combinedscore,
+        overlapping_genes=overlapping_genes,
+        adjusted_pvalue=adjusted_pvalue,
+      )
+      for rank, term, pvalue, zscore, combinedscore, overlapping_genes, adjusted_pvalue, _, _ in results[library]
+    ])
