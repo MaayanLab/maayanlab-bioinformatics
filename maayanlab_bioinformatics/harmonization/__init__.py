ddb8c20b8619345546666c95b0d90b70f1e81cc4
Daniel Clarke <u8sand@gmail.com>
2022-06-27 16:36:41 -0400

    Add IDMapper class

--- maayanlab_bioinformatics/harmonization/__init__.py
+++ maayanlab_bioinformatics/harmonization/__init__.py
@@ -3,3 +3,4 @@
 
 from maayanlab_bioinformatics.harmonization.ncbi_genes import ncbi_genes_fetch, ncbi_genes_lookup
 from maayanlab_bioinformatics.harmonization.transcripts import transcripts_to_genes
+from maayanlab_bioinformatics.harmonization.id_mapper import IDMapper
