004e3fea4a6128a897855235250a56a69e4a61e1
Daniel Clarke <u8sand@gmail.com>
2022-07-22 11:56:55 -0400

    Add np_describe

--- maayanlab_bioinformatics/utils/__init__.py
+++ maayanlab_bioinformatics/utils/__init__.py
@@ -1,5 +1,6 @@
 '''This module contains general utility functions for convenient analysis
 '''
 
+from maayanlab_bioinformatics.utils.describe import np_describe
 from maayanlab_bioinformatics.utils.fetch_save_read import fetch_save_read
 from maayanlab_bioinformatics.utils.merge import merge
