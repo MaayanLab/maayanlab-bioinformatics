e0db1dc5bc061a4dd7a8606fafc6bca0ce56ba1f
Daniel Clarke <u8sand@gmail.com>
2023-07-25 12:34:36 -0400

    [parse.gmt]: Some improvements

--- maayanlab_bioinformatics/parse/gmt.py
+++ maayanlab_bioinformatics/parse/gmt.py
@@ -1,44 +1,66 @@
 import re
-import json
 import math as m
 import pandas as pd
+import contextlib
 import logging
 
-def _try_json_loads(s):
+def _try_load_number(s):
   try:
-    return json.loads(s)
-  except:
-    return s
+    return int(s)
+  except ValueError:
+    pass
+  try:
+    return float(s)
+  except ValueError:
+    pass
+  return s
 
+@contextlib.contextmanager
 def _ensure_fp(fp, mode):
-  if type(fp) == str: return open(fp, mode)
-  else: return fp
-
-_gene_parser = re.compile(r'^(.+?)([,:;](.+))?$')
-def _parse_gene(gene):
-  match = _gene_parser.match(gene.strip())
-  if match and match.group(3):
-    return match.group(1), _try_json_loads(match.group(3))
+  if type(fp) == str:
+    with open(fp, mode) as fh:
+      yield fh
   else:
-    return gene.strip(), 1
+    yield fp
 
-def gmt_read_iter(fh, parse_gene=_parse_gene):
-  fh = _ensure_fp(fh, 'r')
-  for n, line in enumerate(fh):
-    try:
-      term1, term2, genes_str = line.strip().split('\t', maxsplit=2)
-    except ValueError:
-      logging.warn('Ignoring line {}:{} because it seems empty'.format(n, line))
-      continue
-    term = '\t'.join(filter(None, map(str.strip, (term1, term2))))
-    geneset = {
-      k: v
-      for k, v in map(parse_gene, genes_str.split('\t'))
-      if k
-    }
-    yield term, geneset
+def parse_gene_weight(gene):
+  ''' A helper to parse the gmt potentially with numeric weights
+  '''
+  gene, *_weight = re.split(r'[,:;]', gene.strip(), maxsplit=1)
+  if _weight:
+    _weight, = _weight
+    _weight = _try_load_number(_weight)
+    if type(_weight) == str:
+      gene += _weight
+      weight = 1
+    else:
+      weight = _weight
+  else:
+    weight = 1
+  return gene.strip(), weight
 
-def gmt_read_dict(fh, parse_gene=_parse_gene):
+def parse_gene_unweighted(gene):
+  ''' A helper to parse the gmt unweighted
+  '''
+  return gene.strip(), 1
+
+def gmt_read_iter(fh, parse_gene=parse_gene_weight):
+  with _ensure_fp(fh, 'r') as fh:
+    for n, line in enumerate(fh):
+      try:
+        term1, term2, genes_str = line.strip().split('\t', maxsplit=2)
+      except ValueError:
+        logging.warn('Ignoring line {}:{} because it seems empty'.format(n, line))
+        continue
+      term = '\t'.join(filter(None, map(str.strip, (term1, term2))))
+      geneset = {
+        k: v
+        for k, v in map(parse_gene, genes_str.split('\t'))
+        if k
+      }
+      yield term, geneset
+
+def gmt_read_dict(fh, parse_gene=parse_gene_weight):
   ''' Read .gmt files into a dictionary of the form:
   {
     'term_1\tterm_2': {
@@ -60,7 +82,7 @@ def gmt_read_dict(fh, parse_gene=_parse_gene):
     gmt[term].update(**geneset)
   return gmt
 
-def gmt_read_pd(fh, parse_gene=_parse_gene):
+def gmt_read_pd(fh, parse_gene=parse_gene_weight):
   ''' Read .gmt files directly into a data frame.
   '''
   return pd.DataFrame(gmt_read_dict(fh, parse_gene=parse_gene))
@@ -83,18 +105,18 @@ def gmt_write_dict(gmt, fh, serialize_gene_weight_pair=_serialize_gene_weight_pa
     - it should return the serialized gene,weight pair or None if it should be removed
   By default, 0/nans are dropped, 1s result in a gene (crisp), and everything else uses gene,weight.
   '''
-  fh = _ensure_fp(fh, 'w')
-  for term, geneset in gmt.items():
-    if '\t' not in term: serialized_term = term + '\t'
-    else: serialized_term = term
-    serialized_geneset = '\t'.join(filter(None, (
-      serialize_gene_weight_pair(gene, weight)
-      for gene, weight in _ensure_weight(geneset)
-    )))
-    if not serialized_geneset:
-      logging.warn('Ignoring term {} because its geneset seems empty'.format(term))
-      continue
-    print(serialized_term, serialized_geneset, sep='\t', file=fh)
+  with _ensure_fp(fh, 'w') as fh:
+    for term, geneset in gmt.items():
+      if '\t' not in term: serialized_term = term + '\t'
+      else: serialized_term = term
+      serialized_geneset = '\t'.join(filter(None, (
+        serialize_gene_weight_pair(gene, weight)
+        for gene, weight in _ensure_weight(geneset)
+      )))
+      if not serialized_geneset:
+        logging.warn('Ignoring term {} because its geneset seems empty'.format(term))
+        continue
+      print(serialized_term, serialized_geneset, sep='\t', file=fh)
 
 def gmt_write_pd(df, fh, serialize_gene_weight_pair=_serialize_gene_weight_pair):
   ''' Write a pandas dataframe as a gmt, where rows are genes and columns are terms.
