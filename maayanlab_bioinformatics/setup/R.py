805ab3bd69943bc8e30b9d887258acfb02a41a70
Daniel J. B. Clarke <u8sand@gmail.com>
2025-06-04 11:37:41 -0400

    setup.R shouldn't run when being imported

--- maayanlab_bioinformatics/setup/R.py
+++ maayanlab_bioinformatics/setup/R.py
@@ -1,13 +1,20 @@
-import rpy2.robjects as ro
-ro.r('''
-install.packages("R.utils", repos="https://cloud.r-project.org/")
-install.packages("RCurl", repos="https://cloud.r-project.org/")
+'''
+Install R dependencies with
+```
+python -m maayanlab_bioinformatics.setup.R
+```
+'''
+if __name__ == '__main__':
+    import rpy2.robjects as ro
+    ro.r('''
+    install.packages("R.utils", repos="https://cloud.r-project.org/")
+    install.packages("RCurl", repos="https://cloud.r-project.org/")
 
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager", repos="https://cloud.r-project.org/")
+    if (!requireNamespace("BiocManager", quietly = TRUE))
+        install.packages("BiocManager", repos="https://cloud.r-project.org/")
 
-BiocManager::install("DESeq2")
-BiocManager::install("limma")
-BiocManager::install("statmod")
-BiocManager::install("edgeR")
-''')
+    BiocManager::install("DESeq2")
+    BiocManager::install("limma")
+    BiocManager::install("statmod")
+    BiocManager::install("edgeR")
+    ''')
