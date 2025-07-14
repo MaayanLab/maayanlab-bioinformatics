5f61e1a456b2f8f02a536c835a04b3bba03c324a
Daniel J. B. Clarke <u8sand@gmail.com>
2025-07-14 15:59:26 -0400

    Facilitate cache directory override

--- maayanlab_bioinformatics/utils/fetch_save_read.py
+++ maayanlab_bioinformatics/utils/fetch_save_read.py
@@ -1,12 +1,17 @@
 import os
 import pandas as pd
 
-def fetch_save_read(url, file, reader=pd.read_csv, sep=',', **kwargs):
+def fetch_save_read(url, file, cache_dir='', reader=pd.read_csv, sep=',', **kwargs):
   ''' Download file from {url}, save it to {file}, and subsequently read it with {reader} using pandas options on {**kwargs}.
   '''
-  if not os.path.exists(file):
-    if os.path.dirname(file):
-      os.makedirs(os.path.dirname(file), exist_ok=True)
-    df = reader(url, sep=sep, index_col=None)
-    df.to_csv(file, sep=sep, index=False)
-  return pd.read_csv(file, sep=sep, **kwargs)
+  if cache_dir or cache_dir == '':
+    path = file if cache_dir == '' else cache_dir + os.path.sep + file
+    if not os.path.exists(path):
+      if os.path.dirname(path):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+      df = reader(url, sep=sep, index_col=None)
+      df.to_csv(path, sep=sep, index=False)
+    return reader(path, sep=sep, **kwargs)
+  else:
+    return reader(url, sep=sep, **kwargs)
+
